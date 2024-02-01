2a2822b8f4c2463684064e102d376df679fda69f
LucasMoskun <contact@lucasmoskun.com>
2024-02-01 05:47:49 -0800

    remove old counter code

--- endcoin-frontend/src/app/store.ts
+++ endcoin-frontend/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { programReducer } from '../features/program/program-slice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     program: programReducer,
   },
 });
