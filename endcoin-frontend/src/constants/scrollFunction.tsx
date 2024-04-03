473cea31f3c37d79321b0e0d69649a4d5783e64f
arjun <arjunpotter17@gmail.com>
2024-04-03 21:58:59 +0530

    removed commit

--- endcoin-frontend/src/constants/scrollFunction.tsx
+++ endcoin-frontend/src/constants/scrollFunction.tsx
@@ -1,6 +1,5 @@
 export const scrollToDiv = (id: string) => {
   const element = document.getElementById(id);
-  console.log(element);
   if (element) {
     if (element.id === 'hackathon' || element.id === 'whitepaper')
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
