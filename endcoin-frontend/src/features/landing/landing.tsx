c1e31529e71c7ebf2100021ec6a07c058eb7f469
LucasMoskun <contact@lucasmoskun.com>
2024-04-07 17:47:04 +0100

    clean unneeded imports

--- endcoin-frontend/src/features/landing/landing.tsx
+++ endcoin-frontend/src/features/landing/landing.tsx
@@ -1,12 +1,3 @@
-import goal from '../../images/goal.png';
-import AreaChart from '../graph/area-chart';
-import { EReducerState } from '../../app/enum';
-import {
-  selectGraphDataPoints,
-  selectProgramStatus,
-  fetchProgramBalanceAsync,
-} from '../program/program-slice';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import Coin from './Coin';
 import About from './About';
 import Contact from './Contact';
@@ -15,8 +6,6 @@ import WhitePaper from './WhitePaper';
 import DataGraph from './DataGraph';
 
 export default function Landing() {
-  const programStatus: EReducerState = useAppSelector(selectProgramStatus);
-  const dispatch = useAppDispatch();
   return (
     <>
       <div className="flex flex-col bg-[#09090E]">
