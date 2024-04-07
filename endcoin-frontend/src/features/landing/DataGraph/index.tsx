38378ce78dfeeacbce7696ecc666a4c00ff07637
LucasMoskun <contact@lucasmoskun.com>
2024-04-07 19:02:29 +0100

    update data copy

--- endcoin-frontend/src/features/landing/DataGraph/index.tsx
+++ endcoin-frontend/src/features/landing/DataGraph/index.tsx
@@ -13,6 +13,9 @@ const DataGraph = (props: Props) => {
         <p className="text-white text-[30px] endcoin-md:text-[40px] endcoin-xl:text-[48px] text-left font-endcoin-bold">
           Where are we now?
         </p>
+        <p className="text-white text-[20px] endcoin-md:text-[30px] endcoin-xl:text-[38px] text-left font-endcoin-bold">
+          DeFi
+        </p>
         <p className="text-[14px] endcoin-md:text-[18px] text-white">
           Andrew recently completed the 
           <a
@@ -21,19 +24,39 @@ const DataGraph = (props: Props) => {
           >
             web 3 builders alliance (WBA)
           </a>{' '}
-          Q4 Cohort, where we deployed an MVP POC to solana devnet. This version
-          reads sea surface temperature from a data API, which is read by the
-          solana program. This data drives the emission rate of end coin and
-          gaia coin. When temperatures rise more gaia coin than endcoin are
-          created. When temperatures fall, more endcoin than gaiacoin are
-          emitted. When viewed as a pair in an AMM, this draws a “heartbeat” the
-          “pulse on climate” – here is this data, being pulled live from the AMM
-          on devnet, showing the last 30 days of emissions:
+          Q4 Cohort, where we deployed an MVP POC to solana devnet. Using
+          Switchboard, the solana program reads sea surface temperature from a
+          data API. This data drives the emission rate of Endcoin and Gaiacoin.
+          When temperatures rise more Gaiacoin than Endcoin are created. When
+          temperatures fall, more Endcoin than Gaiacoin are created. When viewed
+          as a pair in an AMM, this draws a “heartbeat” the “pulse on climate.”
+          here is this data,from the AMM transactions on devnet, showing 30 days
+          of emissions:
         </p>
 
         <div className="flex flex-col max-w-full">
           <AreaChart />
         </div>
+        <p className="text-[14px] endcoin-md:text-[18px] text-white">
+          We will take this MVP live on mainnet by the end of Q2 2024.
+        </p>
+        <p className="text-white text-[20px] endcoin-md:text-[30px] endcoin-xl:text-[38px] text-left font-endcoin-bold">
+          DePin
+        </p>
+        <p className="text-[14px] endcoin-md:text-[18px] text-white">
+          During the Renaissance hackathon, we built a POC for our DePin sensor.
+          We are now in the process of establishing partnerships and on the
+          campaign trail to raise funding to bring it to commercial production.
+          We intend to have a saleable product by the end of Q3 2024. please
+          visit our{' '}
+          <a
+            className="text-end-button-blue hover:underline"
+            href="https://github.com/pulse-on-climate/endcoin-whitepaper"
+          >
+            whitepaper
+          </a>{' '}
+          to venture down the rabbit hole.
+        </p>
       </div>
     </Layout>
   );
