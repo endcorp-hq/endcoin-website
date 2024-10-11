298282146f554780cfc572a610661e44cb4cab54
arjunpotter17 <arjunpotter17@gmail.com>
2024-10-11 16:53:54 +0530

    made very slight changes

--- endcoin-frontend/src/constants/navbar-items.tsx
+++ endcoin-frontend/src/constants/navbar-items.tsx
@@ -2,25 +2,31 @@ export const NAVBAR_LINKS = [
   {
     name: 'About',
     link: 'mission',
+    id: 1
   },
   {
     name: 'Whitepaper',
     link: 'whitepaper',
+    id: 2
   },
   {
     name: 'Contact',
     link: 'contact',
+    id: 3
   },
   {
     name: '|',
     link: '',
+    id: 4
   },
   {
     name: 'Data',
     link: 'dataGraph',
+    id: 5
   },
   {
     name: 'Swap',
     link: 'swap',
+    id: 6
   },
 ];
