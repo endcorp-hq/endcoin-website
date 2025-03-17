84c4ead643c2a44f640712f5fcd0a43b4cba5648
imetandy <robinson.andrewjames@gmail.com>
2025-03-17 17:49:16 +0800

    updated link

--- endcoin-frontend/src/features/landing/WhitePaper/index.tsx
+++ endcoin-frontend/src/features/landing/WhitePaper/index.tsx
@@ -14,24 +14,23 @@ const WhitePaper = (props: Props) => {
       >
         <div className="text-white flex flex-col gap-y-6 items-center max-w-[830px]">
           <p className="text-[30px] endcoin-md:text-[40px] endcoin-xl:text-[48px] font-endcoin-bold">
-            White Paper
+            Whitepaper
           </p>
           <p className="text-[14px] endcoin-xl:text-[18px] text-center">
             Explore how a decentralized network of ground stations and a novel
             token, appreciating in value with the intensifying effects of
             climate change, can revolutionize funding for green technologies.
-            Delve into the nitty gritty by reading our whitepaper!
           </p>
           <button
             onClick={() =>
               window.open(
-                'https://pulse-on-climate.github.io/endcoin-whitepaper/',
+                'https://whitepaper.endcorp.co/',
                 '_blank',
               )
             }
             className={`rounded border border-end-button-blue text-end-button-blue text-end-button-green text-[12px] endcoin-md:text-[16px] py-2 px-[14.5px] bg-none hover:bg-end-button-blue hover:text-black ${transition}`}
           >
-            Read the White paper
+            Read the Whitepaper
           </button>
         </div>
       </div>
